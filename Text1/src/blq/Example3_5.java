1a04a2db900880ea1e4274948ebd484f6da0453a
bianliqin <2034605883@qq.com>
2020-10-02 22:43:08 +0800

    10.2

--- Text1/src/blq/Example3_5.java
+++ Text1/src/blq/Example3_5.java
@@ -1,4 +1,12 @@
 package blq;
 
 public class Example3_5 {
+    public static void main(String args[]) {
+        long sum = 0, a = 8, itme = a, n = 12, i = 1;
+        for (i = 1; i <= n; i++) {
+            sum = sum + itme;
+            itme = itme * 10 + a;
+        }
+        System.out.println(sum);
+    }
 }
