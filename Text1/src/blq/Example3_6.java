6f277e5f5ad8194a5749d31f260014fbd0f13e1a
bianliqin <2034605883@qq.com>
2020-10-02 22:47:25 +0800

    10.2

--- Text1/src/blq/Example3_6.java
+++ Text1/src/blq/Example3_6.java
@@ -1,4 +1,14 @@
 package blq;
 
 public class Example3_6 {
+    public static void main(String args[]) {
+        double sum = 0, itme = 1;
+        int i = 1, n = 20;
+        while (i <= n) {
+            sum = sum + itme;
+            i = i + 1;
+            itme = itme * (1.0 / i);
+        }
+        System.out.println("sum=" + sum);
+    }
 }
