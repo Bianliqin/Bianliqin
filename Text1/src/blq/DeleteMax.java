e22804c3a2e2ff57fae4cf0e922036a2102c8218
bianliqin <2034605883@qq.com>
2020-10-05 23:42:43 +0800

    10.5

---
+++ Text1/src/blq/DeleteMax.java
@@ -0,0 +1,10 @@
+package blq;
+
+/**
+ * @ClassName DeleteMax
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/5
+ **/
+public class DeleteMax {
+}
---
+++ Text1/src/blq/Example4_1.java
@@ -0,0 +1,22 @@
+package blq;
+
+/**
+ * @ClassName Example4_1
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/5
+ **/
+class XiyoujiRenwu{
+    float height,weight;
+    String head,ear;
+    void speak(String s){
+        System.out.println(s);
+    }
+}
+public class Example4_1 {
+    public static void main(String args[]){
+        XiyoujiRenwu zhubajie,sunwukong;
+        zhubajie = new XiyoujiRenwu();
+        sunwukong = new XiyoujiRenwu();
+    }
+}
