16b0e8a372547ed2f53ad1dd7f078e686157077c
bianliqin <2034605883@qq.com>
2020-10-06 22:02:45 +0800

    10.6

---
+++ Text1/src/blq/Example4_4.java
@@ -0,0 +1,10 @@
+package blq;
+
+/**
+ * @ClassName Example4_4
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/6
+ **/
+public class Example4_4 {
+}
---
+++ Text1/src/blq/Rect.java
@@ -0,0 +1,10 @@
+package blq;
+
+/**
+ * @ClassName Rect
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/6
+ **/
+public class Rect {
+}
