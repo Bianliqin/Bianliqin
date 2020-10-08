6c2c3d72bf0821faeaedee0dc113690bce6a3884
bianliqin <2034605883@qq.com>
2020-10-08 16:48:57 +0800

    10.8

---
+++ Text1/src/blq/Example4_9.java
@@ -0,0 +1,10 @@
+package blq;
+
+/**
+ * @ClassName Example4_9
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/8
+ **/
+public class Example4_9 {
+}
---
+++ Text1/src/blq/Ladder.java
@@ -0,0 +1,13 @@
+package blq;
+
+/**
+ * @ClassName MainClass
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/8
+ **/
+public class MainClass {
+    public static void main(String args[]){
+        Rational r1=new 
+    }
+}
