6aa959e1a4438b42bb1eb3baee75528a1e37e2d7
bianliqin <2034605883@qq.com>
2020-10-10 18:32:56 +0800

    10.10

---
+++ Text1/src/blq/AAA.java
@@ -0,0 +1,10 @@
+package blq;
+
+/**
+ * @ClassName AAA
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/10
+ **/
+public class AAA {
+}
---
+++ Text1/src/blq/Triangle.java
@@ -0,0 +1,10 @@
+package blq;
+
+/**
+ * @ClassName Triangle
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/9
+ **/
+public class Triangle {
+}
