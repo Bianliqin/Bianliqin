816c3d065aa911c996afc567af6c36edd5b2fd18
bianliqin <2034605883@qq.com>
2020-10-10 18:35:32 +0800

    10.10

--- Text1/src/blq/AAA.java
+++ Text1/src/blq/AAA.java
@@ -7,4 +7,14 @@ package blq;
  * @Date 2020/10/10
  **/
 public class AAA {
+    private int money;
+    private int getMoney(){
+        return money;
+    }
+    public static void main(String args[]){
+        AAA exa = new AAA();
+        exa.money = 3000;
+        int m = exa.getMoney();
+        System.out.println("money = " + m);
+    }
 }
