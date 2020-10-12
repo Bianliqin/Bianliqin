232001672f554a241faa25f25fcb70d5376ecd56
bianliqin <2034605883@qq.com>
2020-10-12 10:56:20 +0800

    10.12

---
+++ Text1/src/blq/Rectangle.java
@@ -0,0 +1,39 @@
+package blq;
+
+/**
+ * @ClassName Rectangle
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/12
+ **/
+public class Rectangle {
+    double x,y,width,height;
+    public void setX(double a) {
+        x = a;
+    }
+    public double getX(){
+        return x;
+    }
+    public void setY(double b){
+        y=b;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setWidth(double w) {
+        if (w > 0) {
+            width = w;
+        }
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setHeight(double h) {
+        if (height > 0) {
+            height = h;
+        }
+    }
+    public double getHeight(){
+        return height;
+    }
+}
