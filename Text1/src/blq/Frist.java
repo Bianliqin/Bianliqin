f27526b284e2e303b1356eebada744a242159dd4
bianliqin <2034605883@qq.com>
2020-10-15 17:08:08 +0800

    10.15

---
+++ Text1/src/blq/Frist.java
@@ -0,0 +1,16 @@
+package blq;
+
+/**
+ * @ClassName Frist
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/15
+ **/
+public class Frist {
+    static String s1 = "你好";
+    public static void main(String args[]){
+        String s2 = "Java";
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
