95cdcfa7fe642769178a18508cff3137e6e99ac7
bianliqin <2034605883@qq.com>
2020-10-15 12:55:32 +0800

    10.15

---
+++ Text1/src/blq/HelloJava.java
@@ -0,0 +1,13 @@
+package blq;
+
+/**
+ * @ClassName HelloJava
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/15
+ **/
+public class HelloJava {
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+    }
+}
