98a8e9089d5b3f65eea2205441d75ab5baf957a1
bianliqin <2034605883@qq.com>
2020-10-15 13:04:15 +0800

    10.15

---
+++ Text1/src/blq/Operation.java
@@ -0,0 +1,16 @@
+package blq;
+
+/**
+ * @ClassName Operation
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/15
+ **/
+public class Operation {
+    public static void main(String args[]){
+        System.out.println("5+6="+(5+6));
+        System.out.println("5-6="+(5-6));
+        System.out.println("5*6="+(5*6));
+        System.out.println("5/6="+(5/6));
+    }
+}
---
+++ Text1/src/blq/ShowStudy.java
@@ -0,0 +1,10 @@
+package blq;
+
+/**
+ * @ClassName ShowStudy
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/15
+ **/
+public class ShowStudy {
+}
