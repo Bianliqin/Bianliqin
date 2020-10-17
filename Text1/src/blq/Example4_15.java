1ec1ddff350bb94721b952f99dcd7c2dc204f77a
bianliqin <2034605883@qq.com>
2020-10-17 15:24:55 +0800

    10.17

---
+++ Text1/src/blq/Example4_15.java
@@ -0,0 +1,19 @@
+package blq;
+
+/**
+ * @ClassName Example4_15
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/17
+ **/
+class B{
+    public void hello(){
+        System.out.println("你好");
+    }
+}
+public class Example4_15 {
+    public static void main(String args[]){
+        B a = new B();
+        a.hello();
+    }
+}
