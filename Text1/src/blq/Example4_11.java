668b9e99dd1e5f5b6f0bf24083251be5138e4428
bianliqin <2034605883@qq.com>
2020-10-17 15:13:32 +0800

    10.17

--- Text1/src/blq/Example4_11.java
+++ Text1/src/blq/Example4_11.java
@@ -7,4 +7,9 @@ package blq;
  * @Date 2020/10/17
  **/
 public class Example4_11 {
+    public static void main(String args[]){
+        Student stu = new Student(10201);
+        stu.speak();
+        System.out.println("主类的包名也是tom.jiafei");
+    }
 }
